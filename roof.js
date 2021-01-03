e9f8d634b2a13c4f40b27ce3dc855fea9e8ab62b
hh210806 <73598433+hh210806@users.noreply.github.com>
2021-01-03 20:22:39 +0530

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,18 @@
+class roof {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      strokeWeight(5);
+      rectMode(CENTER);
+      fill("blue");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  }
